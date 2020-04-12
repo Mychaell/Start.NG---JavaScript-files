1e8c6dd3da5e4cbd6eff3462955626e35162f9fd
Mychaell <emmaway3@gmail.com>
2020-04-13 00:02:42 +0100

    Did some changes here

--- script.js
+++ script.js
@@ -2,9 +2,3 @@ let name = "Michael Effiong: ";
 let courses = ["Html",  "css",  "javascript "];
 console.log(name + courses);
 
-for (let i=1; i<200; i++) { 
-		if(i % 2 !== 0) { 
-			console.log(i);
-		}
-	}
-
